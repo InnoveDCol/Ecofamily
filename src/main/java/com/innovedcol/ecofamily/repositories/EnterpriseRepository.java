4e00556bb45eb43d0bed6e005deeec99636d5c62
JeyZG <jdzg1124@gmail.com>
2022-09-06 20:55:49 -0500

    Implementado CRUD completo para entidad Enterprise, incluye validaciones de existencia de registros.

--- src/main/java/com/innovedcol/ecofamily/repositories/EnterpriseRepository.java
+++ src/main/java/com/innovedcol/ecofamily/repositories/EnterpriseRepository.java
@@ -3,8 +3,6 @@ package com.innovedcol.ecofamily.repositories;
 import com.innovedcol.ecofamily.entities.Enterprise;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-//TODO: Quitar comentarios de la lineas siguiente y reemplazar la linea 8 por la 7
-//public interface EnterpriseRepository extends JpaRepository<Enterprise, Long> {
-public interface EnterpriseRepository {
+public interface EnterpriseRepository extends JpaRepository<Enterprise, Long> {
 
 }
