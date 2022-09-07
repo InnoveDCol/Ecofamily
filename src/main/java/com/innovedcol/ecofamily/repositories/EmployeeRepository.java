e0797967a69c2bbe7590b30286021feef16c033b
JeyZG <jdzg1124@gmail.com>
2022-09-06 20:17:26 -0500

    Se crea la entidad Profile con anotaciones. Se crean los repositorios para las 4 entidades.

---
+++ src/main/java/com/innovedcol/ecofamily/repositories/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.innovedcol.ecofamily.repositories;
+
+import com.innovedcol.ecofamily.entities.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//TODO: Quitar comentarios de la lineas siguiente y reemplazar la linea 8 por la 7
+//public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+public interface EmployeeRepository {
+}
---
+++ src/main/java/com/innovedcol/ecofamily/repositories/ProfileRepository.java
@@ -0,0 +1,10 @@
+package com.innovedcol.ecofamily.repositories;
+
+import com.innovedcol.ecofamily.entities.Profile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//TODO: Quitar comentarios de la lineas siguiente y reemplazar la linea 8 por la 7
+// public interface ProfileRepository extends JpaRepository<Profile, String> {
+public interface ProfileRepository{
+
+}
