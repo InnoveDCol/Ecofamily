5263e3434c2d3b7ca622c2e907d6acbe0af3a021
JeyZG <jdzg1124@gmail.com>
2022-09-16 23:38:38 -0500

    Se elimina todo lo relacionado a Profile ya que se fusiona con Employee y se actualiza entidad Employee

--- src/main/java/com/innovedcol/ecofamily/entities/Employee.java
+++ src/main/java/com/innovedcol/ecofamily/entities/Employee.java
@@ -1,70 +1,72 @@
 package com.innovedcol.ecofamily.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.innovedcol.ecofamily.enums.RoleEmployeeEnum;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.Date;
+import java.util.*;
 
 @Entity
 @Table(name = "Employee")
 public class Employee {
 
     @Id
-    @Column (unique = true)
-    @GeneratedValue (strategy = GenerationType.AUTO)
+    //@Column (unique = true)
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column (nullable = false, length = 50)
     private String name;
 
-    @Column (nullable = false, length = 50,unique = true)
+    @Column (nullable = false, length = 50, unique = true)
     private String email;
 
-    @OneToOne (cascade= {CascadeType.ALL})//Corregido.
-    @JoinColumn (name = "Profile",nullable = false)
-    private Profile profile;
+    @Column(nullable = false)
+    private String phone;
 
-    @OneToMany(cascade= {CascadeType.ALL})
     @Enumerated(EnumType.STRING)
-    @Column (nullable = false)
+    @Column (name="role", nullable = false)
     private RoleEmployeeEnum rol;
 
-    @ManyToOne(cascade= {CascadeType.ALL})//Corregido.
-    @JoinColumn(name = "Enterprise_ID")
+    @Column(nullable = false)
+    private String image;
+
+    @ManyToOne
+    @JoinColumn(name = "enterprise")
+    @JsonIgnore
     private Enterprise enterpriseContratante;
 
-    // TODO: Atributo para listar los movimientos de dinero de una empresa.
     @OneToMany(mappedBy = "user")//Corregido.
-    //@Column (nullable = true, length = 50)
-    private ArrayList<Transaction> transactionList;
+    private List<Transaction> transactionList;
 
-    @Column (nullable = false)
-    private Date updatedAt;
-
+    @Temporal(TemporalType.DATE)
     @Column (nullable = false)
     private Date createdAt;
 
+    @Temporal(TemporalType.DATE)
+    @Column (nullable = true)
+    private Date updatedAt;
     //MÉTODOS
 
     //Constructores:
+
     public Employee() {
     }
 
-    public Employee(Long id, String name, String email, Profile profile, RoleEmployeeEnum rol, Enterprise enterpriseContratante, ArrayList<Transaction> transactionList, Date updatedAt, Date createdAt) {
+    public Employee(Long id, String name, String email, String phone, RoleEmployeeEnum rol, String image, Enterprise enterpriseContratante, List<Transaction> transactionList, Date createdAt, Date updatedAt) {
         this.id = id;
         this.name = name;
         this.email = email;
-        this.profile = profile;
+        this.phone = phone;
         this.rol = rol;
+        this.image = image;
         this.enterpriseContratante = enterpriseContratante;
         this.transactionList = transactionList;
-        this.updatedAt = updatedAt;
         this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
     }
 
-    //Getters-Setters:
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
@@ -88,12 +90,12 @@ public class Employee {
         this.email = email;
     }
 
-    public Profile getProfile() {
-        return profile;
+    public String getPhone() {
+        return phone;
     }
 
-    public void setProfile(Profile profile) {
-        this.profile = profile;
+    public void setPhone(String phone) {
+        this.phone = phone;
     }
 
     public RoleEmployeeEnum getRol() {
@@ -104,6 +106,14 @@ public class Employee {
         this.rol = rol;
     }
 
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
     public Enterprise getEnterpriseContratante() {
         return enterpriseContratante;
     }
@@ -112,20 +122,12 @@ public class Employee {
         this.enterpriseContratante = enterpriseContratante;
     }
 
-    public ArrayList<Transaction> getTransactionList() {
+    public List<Transaction> getTransactionList() {
         return transactionList;
     }
 
-    public void setTransactionList(ArrayList<Transaction> transactionList) {
+    public void setTransactionList(List<Transaction> transactionList) {
         this.transactionList = transactionList;
-    }
-
-    public Date getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(Date updatedAt) {
-        this.updatedAt = updatedAt;
     }
 
     public Date getCreatedAt() {
@@ -136,19 +138,11 @@ public class Employee {
         this.createdAt = createdAt;
     }
 
-    @Override
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
 
-    public String toString() {
-        return "Employee{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                ", profile=" + profile +
-                ", rol=" + rol +
-                ", enterpriseContratante=" + enterpriseContratante +
-                ", transactionList=" + transactionList +
-                ", updatedAt=" + updatedAt +
-                ", createdAt=" + createdAt +
-                '}';
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
     }
 }
