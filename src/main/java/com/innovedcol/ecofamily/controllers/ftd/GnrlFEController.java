92059f0144e20c7c59d3deba147d47fc704a4edc
JeyZG <jdzg1124@gmail.com>
2022-09-27 17:36:45 -0500

    Inicio de implementacion del login con Auth0

--- src/main/java/com/innovedcol/ecofamily/controllers/ftd/GnrlFEController.java
+++ src/main/java/com/innovedcol/ecofamily/controllers/ftd/GnrlFEController.java
@@ -1,7 +1,10 @@
 package com.innovedcol.ecofamily.controllers.ftd;
 
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,8 +12,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class GnrlFEController {
 
     @RequestMapping(value= {"/index","/"})
-    public String index() {
+    public String index(Model model, @AuthenticationPrincipal OidcUser principal) {
         return "index";
     }
+
 
 }
